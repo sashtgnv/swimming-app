69509fb3c42423caf033ed0c334aafa75b34be91
Sasha Tuganov <alekstuganov@yandex.ru>
2024-11-02 21:56:09 +0300

    empty project

---
+++ src/main/java/com/example/swimming_championships_app/SwimmingChampionshipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.swimming_championships_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingChampionshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingChampionshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swimming_championships_app/SwimmingChampionshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swimming_championships_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimmingChampionshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
